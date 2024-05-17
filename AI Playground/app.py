10ab5f13374ba7780dc9a92e1c3342d2e87dba91
Nafis Rayan <nafisrayan123@gmail.com>
2024-05-17 17:16:19 +0600

    "Added 'Custom Models' option to model selectbox and updated model key input logic"

--- AI Playground/app.py
+++ AI Playground/app.py
@@ -14,7 +14,7 @@ st.title('👀 AI Playground ')
 
 st.text('Web Scraping with Pandas and Streamlit, Gemini, Mistral, and Phi-3')
 
-Model = st.selectbox("Select your prefered model:", ["GEMINI", "MISTRAL8X", "PHI-3"])
+Model = st.selectbox("Select your prefered model:", ["GEMINI", "MISTRAL8X", "PHI-3", "Custom Models"])
 
 if Model == "GEMINI":
     tkey = st.text_input("Your Token or API key here:", "")
@@ -77,8 +77,10 @@ else:
 
     if Model == "MISTRAL8X":
         mkey= "mistralai/Mixtral-8x7B-Instruct-v0.1"
-    else:
+    elif Model == "PHI-3":
         mkey = "microsoft/Phi-3-mini-4k-instruct"
+    else:
+        mkey = st.text_input("Your HuggingFace Model String here:", "")
 
     def format_prompt(message, history):
         prompt = ""
